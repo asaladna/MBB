dd27dcf6ac35692bbbc932a818dfb0a18a1eeff1
Alex Saladna <asaladna@smu.edu>
2016-03-29 18:54:41 -0500

    buttons fix

--- pocketGains/www/js/controllers.js
+++ pocketGains/www/js/controllers.js
@@ -44,9 +44,8 @@ angular.module('starter.controllers', [])
 
 
 .controller("ExampleController", function($scope) {
- 
     $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-    $scope.series = ['Series A', 'Series B'];
+    $scope.series = ['Cardio', 'Weight'];
     $scope.data = [
         [65, 59, 80, 81, 56, 55, 40],
         [28, 48, 40, 19, 86, 27, 90]
