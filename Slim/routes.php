7e7284c2061882e1abfe98ab2679466afffd8d18
Justin Suh <jsuh@smu.edu>
2016-03-29 20:13:20 -0500

    routes.php
    added routes.php

---
+++ Slim/routes.php
@@ -0,0 +1,123 @@
+<?php
+// Routes
+
+$app->get('/achievements', 
+	function ($request, $response, $args) {
+    try {
+    	$db = $this->api_login;
+ 
+ 		//FIX SQL STATEMENT FOR NEWLY UPDATED DB
+        $query = $db->prepare("SELECT * FROM Achievements");
+        $query->execute();
+
+        $arr = $query->fetchAll(PDO::FETCH_ASSOC);
+ 
+        if($arr) {
+            return $response->write(json_encode($arr));
+            $db = null;
+        } 
+        else {
+            throw new PDOException('No records found.');
+        }
+ 
+    } catch(PDOException $e) {
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }
+});
+
+$app->get('/achievements/{user_id}', 
+	function ($request, $response, $args) {
+    try {
+    	$db = $this->api_login;
+ 
+ 		//FIX SQL STATEMENT FOR NEWLY UPDATED DB
+        $query = $db->prepare("SELECT * ");
+        $query->execute(array('user_id' => ));
+
+        $arr = $query->fetchAll(PDO::FETCH_ASSOC);
+ 
+        if($arr) {
+            return $response->write(json_encode($arr));
+            $db = null;
+        } 
+        else {
+            throw new PDOException('No records found.');
+        }
+ 
+    } catch(PDOException $e) {
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }
+});
+
+$app->get('/completedAchievement', 
+	function ($request, $response, $args) {
+    try {
+    	$db = $this->api_login;
+ 
+ 		//FIX SQL STATEMENT FOR NEWLY UPDATED DB
+        $query = $db->prepare(/*SQL STATEMENT GOES HERE*/);
+        $query->execute();
+
+        $arr = $query->fetchAll(PDO::FETCH_ASSOC);
+ 
+        if($arr) {
+            return $response->write(json_encode($arr));
+            $db = null;
+        } 
+        else {
+            throw new PDOException('No records found.');
+        }
+ 
+    } catch(PDOException $e) {
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }
+});
+
+$app->get('/leaderboards', 
+	function ($request, $response, $args) {
+    try {
+    	$db = $this->api_login;
+ 
+ 		//FIX SQL STATEMENT FOR NEWLY UPDATED DB1111
+        $query = $db->prepare(/*SQL STATEMENT GOES HERE*/);
+        $query->execute();
+
+        $arr = $query->fetchAll(PDO::FETCH_ASSOC);
+ 
+        if($arr) {
+            return $response->write(json_encode($arr));
+            $db = null;
+        } 
+        else {
+            throw new PDOException('No records found.');
+        }
+ 
+    } catch(PDOException $e) {
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }
+});
+
+$app->get('//userPoints/{user_id}', 
+	function ($request, $response, $args) {
+    try {
+    	$db = $this->api_login;
+ 
+ 		//FIX SQL STATEMENT FOR NEWLY UPDATED DB
+        $query = $db->prepare(/*SQL STATEMENT GOES HERE*/);
+        $query->execute();
+
+        $arr = $query->fetchAll(PDO::FETCH_ASSOC);
+ 
+        if($arr) {
+            return $response->write(json_encode($arr));
+            $db = null;
+        } 
+        else {
+            throw new PDOException('No records found.');
+        }
+ 
+    } catch(PDOException $e) {
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }
+});
+?>
