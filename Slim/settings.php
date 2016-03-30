974c337fe818afe92c954b6d09896eca98f76559
Justin Suh <jsuh@smu.edu>
2016-03-29 20:08:19 -0500

    settings.php and dependencies.php
    adding settings and dependencies

---
+++ Slim/settings.php
@@ -0,0 +1,25 @@
+<?php
+return [
+    'settings' => [
+        'displayErrorDetails' => true, // set to false in production
+
+        // Renderer settings
+        'renderer' => [
+            'template_path' => __DIR__ . '/../templates/',
+        ],
+
+        // Monolog settings
+        'logger' => [
+            'name' => 'slim-app',
+            'path' => __DIR__ . '/../logs/app.log',
+        ],
+
+        'api_login' => [ 
+            'username' => 'api_test',         
+            'password' => 'qwe123',              
+            'host' => 'localhost',              
+            'dbname' => 'pocketgains',          
+            'db' => 'mysql' 
+        ],
+    ],
+];
