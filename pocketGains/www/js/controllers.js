351b9e09e41e6789c0944a0e2021d1586f506eaf
Alex Saladna <asaladna@smu.edu>
2016-04-05 18:01:13 -0500

    Bar fixed (again)

--- pocketGains/www/js/controllers.js
+++ pocketGains/www/js/controllers.js
@@ -259,6 +259,12 @@ angular.module('starter.controllers', [])
 
 .controller('HomeTabCtrl', function($scope) {
   console.log('HomeTabCtrl');
+    $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+    $scope.series = ['Cardio', 'Weight'];
+    $scope.data = [
+        [65, 59, 80, 81, 56, 55, 40],
+        [28, 48, 40, 19, 86, 27, 90]
+    ];
 })
 
 .controller('AchieveCtrl', function($scope, $http) {
