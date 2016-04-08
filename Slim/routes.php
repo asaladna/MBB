735c6911ede0cb85d2edf08976622329c8d60ea1
Justin Suh <jsuh@smu.edu>
2016-04-08 05:11:43 -0500

    Update routes.php
    adding endpoints. Google dos will tell wchih ones are testable

--- Slim/routes.php
+++ Slim/routes.php
@@ -3,10 +3,6 @@
 header('Access-Control-Allow-Origin: *');  
 // Routes
 
-$app->get('/test', function ($request, $response, $args) {
-	return $response->write("Welcome to pocketgains");
-});
-
 $app->get('/achievements', 
 	function ($request, $response, $args) {
     try {
@@ -73,8 +69,8 @@ $app->post('/completedAchievement',
 	function ($request, $response, $args) {
         $db = $this->api_login;
     
-        $parms = $request->getParsedbody();
-        if ($parms) {
+        $parms = $request->getParsedBody();
+        if($parms) {
             echo "Has  already parsed body";
         }
         if(!$parms) {
@@ -93,12 +89,42 @@ $app->post('/completedAchievement',
             );
 });
 
-$app->get('/leaderboards', 
+$app->get('/getLeaders/top_{x}', 
 	function ($request, $response, $args) {
     try {
     	$db = $this->api_login;
+
+        $query = $db->prepare(
+            'SELECT u.user_id, u.username, p.cardio 
+            FROM User u, Points p
+            ORDER BY p.cardio
+            DESC LIMIT :x');
+        $query->execute(
+            array(
+                'x' => (int)$args['x']
+                )
+            );
+
+        $arr = $query->fetchAll(PDO::FETCH_ASSOC);
  
- 		//FIX SQL STATEMENT FOR NEWLY UPDATED DB1111
+        if($arr) {
+            return $response->write(json_encode($arr));
+            $db = null;
+        } 
+        else {
+            throw new PDOException('No records found.');
+        }
+ 
+    } catch(PDOException $e) {
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }
+});
+
+$app->get('/getLeaderboardUser/{user_id}', 
+    function ($request, $response, $args) {
+    try {
+        $db = $this->api_login;
+ 
         $query = $db->prepare(/*SQL STATEMENT GOES HERE*/);
         $query->execute();
 
@@ -117,14 +143,27 @@ $app->get('/leaderboards',
     }
 });
 
-$app->get('/userPoints/{user_id}', 
+$app->get('/workout/{user_id}/{workout_id}', 
 	function ($request, $response, $args) {
     try {
     	$db = $this->api_login;
  
- 		//FIX SQL STATEMENT FOR NEWLY UPDATED DB
-        $query = $db->prepare(/*SQL STATEMENT GOES HERE*/);
-        $query->execute();
+ 		 $query = $db->prepare(
+            'SELECT t.name, wh.time_stamp, wh.weight, wh.sets, wh.reps
+                FROM User u, Workout_History wh, Workout w, Is_Type it, Types t
+                WHERE u.user_id = :user_id
+                AND :user_id = wh.User_user_id
+                AND wh.Workout_workout_id = :workout_id
+                AND :workout_id = w.workout_id
+                AND w.workout_id = it.Workout_workout_id
+                AND it.Types_type_id = t.type_id;'
+            );
+        $query->execute(
+            array(
+                'user_id' => $args['user_id'],
+                'workout_id' => $args['workout_id']
+                )
+            );
 
         $arr = $query->fetchAll(PDO::FETCH_ASSOC);
  
@@ -137,6 +176,39 @@ $app->get('/userPoints/{user_id}',
         }
  
     } catch(PDOException $e) {
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }
+});
+
+$app->get('/favorites/{user_id}', 
+    function ($request, $response, $args) {
+    try {
+        $db = $this->api_login;
+
+        $query = $db->prepare(
+            'SELECT f.fav_id, f.Workout_workout_id, f.reps, f.sets, f.weight, f.duration
+                FROM Faved_workouts f, User u
+                WHERE u.user_id = :user_id
+                AND u.user_id = f.User_user_id;'
+            );
+        $query->execute(
+            array(
+                'user_id' => $args['user_id']
+                )
+            );
+
+        $arr = $query->fetchAll(PDO::FETCH_ASSOC);
+ 
+        if($arr) {
+            return $response->write(json_encode($arr));
+            $db = null;
+        } 
+        else {
+            throw new PDOException('No records found.');
+        }
+ 
+    } 
+    catch(PDOException $e) {
         echo '{"error":{"text":'. $e->getMessage() .'}}';
     }
 });
