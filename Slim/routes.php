fa8b25fd846fa1f48a44f1a7d52c0a64fa02d207
Justin Suh <jsuh@smu.edu>
2016-04-12 19:23:47 -0500

    Update routes.php
    trying something

--- Slim/routes.php
+++ Slim/routes.php
@@ -1,6 +1,7 @@
 <?php
 
 header('Access-Control-Allow-Origin: *');  
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 // Routes
 
 // Creates an acccount and adds it to the database then takes the user
