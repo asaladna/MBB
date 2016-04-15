11fa8758253e7dbcd4004fca39aa54618775ec8f
wesleysmith2626 <wesleysmith2626@gmail.com>
2016-04-15 22:17:56 +0000

    added /login, /workouts/{type}, /workout/{workout_id}, /getSuggestedWorkouts/{user_id}, /getHistory/{user_id} endpoints and commented out /addCompletedWorkout endpoint

--- Slim/routes.php
+++ Slim/routes.php
@@ -71,6 +71,76 @@ $app->post('/createNewUser', function ($request, $response, $args) {
     }                
 });
 
+// Queries db to see if entered credentials are correct
+// logs user in if proper credentials entered, returns error message if not
+$app->post('/login', function ($request, $response, $args) {
+    // assumes fields aren't blank from client side
+    // also assumes that the user has not logged in recently (session expired)
+    if (isset($_POST['login']))
+    {
+        // retreive user information from html page
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        // protect against sql injection using stripslashes and parameterized queries
+        $username = stripslashes($username);
+        $password = stripslashes($password);
+
+        // connect to pocketgains db
+        // change dbConn to api_login for testing server
+        $db = $this->dbConn;
+
+        try
+        {
+            if ($db)
+            {
+                // grab username and password from db
+                $query = $db->prepare("SELECT username, password FROM User WHERE username = :username
+                    LIMIT 1");
+                $query->execute(array('username' => $username));
+        
+                // ensures one result is returned
+                if ($query->rowCount() == 1)
+                {
+                    //retrieve query results
+                    $result = $query->fetchAll();
+                    $hash = "";
+
+                    foreach($result as $row)
+                        $hash = $row['password'];
+        
+                    // verify passwords match
+                    if (password_verify($password, $hash))
+                    {
+                        // create a new session for the user and store session id in db
+                        session_start();
+                        $session_id = session_id();
+                        // assign the username to the session
+                        $_SESSION['username'] = $username;
+
+                        $query = $db->prepare("UPDATE User SET session_id = :session_id
+                            WHERE username = :username");
+                        $query->execute(array('session_id' => $session_id, 'username' => $username));
+
+                        // go to user dashboard
+                        return $this->renderer->render($response, 'dashboard.html', $args);
+                    }
+                    else
+                        throw new PDOException("invalid username or password");
+                }
+                else
+                    throw new PDOException("invalid username or password");
+            }
+            else
+                throw new PDOException("could not connect to db");
+        }
+        catch (PDOException $e)
+        {
+            echo '{"error":{"text":' . $e->getMessage() .'}}';
+        }
+    }
+});
+
 $app->get('/achievements', 
 	function ($request, $response, $args) {
     try {
@@ -366,6 +436,7 @@ $app->post('/userData',
 		}
 });
 
+/*
 $app->post('/addCompletedWorkout',
     function ($request, $response, $args) {
 			$db = $this->api_login;
@@ -381,7 +452,6 @@ $app->post('/addCompletedWorkout',
 			$duration = $_POST['duration'];
 
 
-/*
 			//TEST CASE HARDCODED TEST USER
 			$user_id = 12;
 //			$title = "Humbug";
@@ -389,7 +459,6 @@ $app->post('/addCompletedWorkout',
 			$reps = 15156;
 			$weight = 9001;
 			$duration = NULL;
-*/
 
 			$query = $db->prepare(
 			"INSERT INTO Workout_History(User_user_id, Workout_workout_id, sets, reps, weight,
@@ -403,6 +472,7 @@ $app->post('/addCompletedWorkout',
 						)
 			);
 });
+*/
 
 $app->get('/workoutTypes',
     function ($request, $response, $args) {
@@ -540,6 +610,151 @@ $app->get('/workoutDaysback/{user_id}/{start}/{end}',
 			echo '{"error":{"text":'. $e->getMessage() .'}}';
 		}
 
+});
+
+// Displays a list of all the workouts for a particular workout type
+$app->get('/workouts/{type}', function ($request, $response, $args) {
+    // get type id
+    $type = $args['type'];
+
+    try
+    {
+        // connect to pocketgains db;
+        // change dbConn to api_login for testing server
+        $db = $this->dbConn;
+
+        if ($db)
+        {
+            $query = $db->prepare("SELECT DISTINCT w.workout_id, w.title, w.desc FROM Workout w, Is_Type i
+                WHERE i.Types_type_id = :type AND i.Workout_workout_id = w.workout_id");
+            $query->execute(array('type' => $type));
+
+            $result = $query->fetchAll(PDO::FETCH_ASSOC);
+
+            if ($result)
+            {
+                return $response->write(json_encode($result));
+            }
+            else
+                throw new PDOException("no results found");
+        }
+        else
+            throw new PDOException("could not connect to db");
+    }
+    catch (PDOException $e)
+    {
+        echo '{"error":{"text"' . $e->getMessage() . '}}';
+    }    
+});
+
+// Displays the workout id, title, desc, and type of a particular workout
+$app->get('/workout/{workout_id}', function ($request, $response, $args) {
+    // get workout id
+    $workout_id = $args['workout_id'];
+
+    try
+    {
+        // connect to pocketgains db
+        // change dbConn to api_login for testing server
+        $db = $this->dbConn;
+
+        if ($db)
+        {
+            $query = $db->prepare("SELECT w.workout_id, w.title, w.desc, t.name
+                FROM Workout w, Types t, Is_Type i WHERE w.workout_id = :workout_id
+                AND i.Workout_workout_id = :workout_id AND i.Types_type_id = t.type_id");
+            $query->execute(array('workout_id' => $workout_id, 'workout_id' => $workout_id));
+
+            $result = $query->fetchAll(PDO::FETCH_ASSOC);
+
+            if ($result)
+            {
+                return $response->write(json_encode($result));
+            }
+            else
+                throw new PDOException("no results found");
+        }
+        else
+            throw new PDOException("could not connect to db");
+    }
+    catch (PDOException $e)
+    {
+        echo '{"error":{"text"' . $e->getMessage() . '}}';
+    }
+});
+
+// Displays suggested workouts for the logged in user
+$app->get('/getSuggestedWorkouts/{user_id}', function($request, $response, $args) {
+    // get user id
+    $user_id = $args['user_id'];
+
+    try
+    {
+        // connect to pocketgains db
+        // change dbConn to api_login for testing server
+        $db = $this->dbConn;
+
+        if ($db)
+        {
+            // assuming this query is correct, no dummy data in Suggested_Workouts table
+            // will add some later to check if query works properly
+            $query = $db->prepare("SELECT w.title, s.sets, s.reps, s.weight, s.duration
+                FROM Suggested_Workouts s, Workout w WHERE s.User_user_id = :user_id
+                AND s.Workout_workout_id = w.workout_id");
+            $query->execute(array('user_id' => $user_id));
+
+            $result = $query->fetchAll(PDO::FETCH_ASSOC);
+
+            if ($result)
+            {
+                return $response->write(json_encode($result));
+            }
+            else
+                throw new PDOException("no results found");
+        }
+        else
+            throw new PDOException("could not connect to db");
+    }
+    catch (PDOException $e)
+    {
+        echo '{"error":{"text"' . $e->getMessage() . '}}';
+    }
+});
+
+// Displays the workout history of the logged in user
+$app->get('/getHistory/{user_id}', function ($request, $response, $args) {
+    // get user id
+    $user_id = $args['user_id'];
+
+    try
+    {
+        // connect to pocketgains db
+        // change dbConn to api_login for testing server
+        $db = $this->dbConn;
+
+        if ($db)
+        {
+            $query = $db->prepare("SELECT h.hist_id, h.time_stamp, h.duration, h.reps, h.weight
+                FROM Workout_History h, Workout w WHERE h.User_user_id = :user_id
+                AND w.workout_id = h.Workout_workout_id");
+            $query->execute(array('user_id' => $user_id));
+
+            $result = $query->fetchAll(PDO::FETCH_ASSOC);
+
+            if ($result)
+            {
+                return $response->write(json_encode($result));
+            }
+            else
+                throw new PDOException("no results found");
+        }
+        else
+            throw new PDOException("could not connect to db");
+    }
+    catch (PDOException $e)
+    {
+        echo '{"error":{"text"' . $e->getMessage() . '}}';
+    }
 });
 
 ?>
