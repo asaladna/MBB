3cbbbcbf933e149adda0a77b75e550fb285dd3e7
wesleysmith2626 <wesleysmith2626@gmail.com>
2016-04-26 01:23:40 +0000

    added cardioPref back into /createNewUser

--- Slim/routes.php
+++ Slim/routes.php
@@ -14,6 +14,7 @@ $app->post('/createNewUser', function ($request, $response, $args) {
     	$password = $params['password'];
     	$sex = $params['sex'];
     	$goal = $params['goal'];
+        $cardioPref = $params['cardioPref'];
     	$exp = 0;
     	$fav_arm_id = $params['arms'];
     	$fav_leg_id = $params['legs'];
@@ -36,11 +37,12 @@ $app->post('/createNewUser', function ($request, $response, $args) {
     		{
     			// insert user info into db
     			$query = $db->prepare("INSERT into User (username, password, sex, goal, exp)
-    				values ('$username', '$password', '$sex', '$goal', $exp)");
+    				values ('$username', '$password', '$sex', '$goal', '$cardioPref', $exp)");
     			$query->bindParam(':username', $username);
     			$query->bindParam(':password', $password);
     			$query->bindParam(':sex', $sex);
     			$query->bindParam(':goal', $goal);
+                $query->bindParam(':cardioPref', $cardioPref);
     			$query->bindParam(':exp', $exp);
     			$query->execute();
     		}
